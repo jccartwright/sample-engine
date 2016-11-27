9a8e668469c35a9511b48157f3847f63479a6891
John Cartwright <john.c.cartwright@comcast.net>
2016-11-26 22:30:56 -0700

    add random sleep time to simulate processing

--- src/main/groovy/ncei/gis/ProcessingService.groovy
+++ src/main/groovy/ncei/gis/ProcessingService.groovy
@@ -7,8 +7,15 @@ import org.springframework.stereotype.Service
 @Log4j
 @Service
 class ProcessingService {
+    def random = new Random()
+    def counter = 0
+
     String processMessage(String content) {
-        log.debug "processing content: ${content}..."
+        //simulate processing that takes up to 30 seconds
+        def processingTime = random.nextInt(30)
+        log.debug "${counter} - processing content: ${content}. Expected duration is ${processingTime} seconds\n"
+        counter++
+        Thread.sleep(processingTime * 1000)
         return "success"
     }
 }
